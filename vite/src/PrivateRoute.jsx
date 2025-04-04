8f346ffdfad573b0988b7a5ad686cd1099125be4
tanubhavjuneja <tanubhavj@gmail.com>
2025-04-04 20:40:16 +0530

    Chnage vite to react

--- vite/src/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-
-function PrivateRoute({ children }) {
-  const isAuthenticated = localStorage.getItem('isAuthenticated') === 'true';
-  return isAuthenticated ? children : <Navigate to="/admin" />;
-}
-
-export default PrivateRoute;
--- vite/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-function App() {
-  return <h1>Welcome to the Main Page</h1>;
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
